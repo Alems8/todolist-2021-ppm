cb00681b6310e9e008b2890420a734885b44cca6
Alessio <alessio.masi@stud.unifi.it>
2022-05-26 09:26:58 +0200

    Inizializzato

---
+++ todolist-2021-ppm/server/config.php
@@ -0,0 +1,7 @@
+<?php
+	// defines database connection data
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'todolist2022');
+	define('DB_PASSWORD', 'todolist202');
+	define('DB_DATABASE', 'todolist2022'); 
+?>
